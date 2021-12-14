a3b673701a7802fb59c663d25229bcadff85e37e
Harinath <kathiarun3630@gmail.com>
2021-12-14 20:06:28 +0530

    comparison operators

---
+++ index.js
@@ -0,0 +1,28 @@
+var num1=10;
+var num2=20;
+var num3=30;
+
+
+//+=
+num1 +=11;
+console.log(num1)
+
+//-=
+num2 -=10;
+console.log(num2)
+
+
+//*=
+num3 *=5;
+console.log(num3)
+
+// /=
+num1 /=7;
+console.log(num1)
+
+
+// %=
+num3 %=7;
+console.log(num3)
+
+
